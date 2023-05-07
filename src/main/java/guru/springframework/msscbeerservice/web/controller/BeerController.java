5aa07f2a72b461b2972626f5552f438b4afac981
Sreeram Ramesh Sharma <sreeram.rameshsharma@epssw.com>
2023-05-07 18:50:13 +0530

    Add validation and error handling to controller. Closes #9.

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package guru.springframework.msscbeerservice.web.controller;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,13 +20,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
         //impl to be done
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId,@RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerId,@RequestBody @Validated BeerDto beerDto){
 
         return  new ResponseEntity(HttpStatus.NO_CONTENT);
     }
